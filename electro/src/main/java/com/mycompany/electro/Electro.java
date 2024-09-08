0cb5e9a01982b912d9f5ee26c5547d8b1ade8fdc
lusmax <lusmax0128@gmail.com>
2024-09-09 00:00:14 +0200

    create instances of Lavadora and Television

--- electro/src/main/java/com/mycompany/electro/Electro.java
+++ electro/src/main/java/com/mycompany/electro/Electro.java
@@ -1,4 +1,5 @@
 package com.mycompany.electro;
+
 import paq.*;
 
 /**
@@ -9,7 +10,16 @@ public class Electro {
 
     public static void main(String[] args) {
         Electrodomestico MisElectrodomesticos[] = new Electrodomestico[6];
-       
-        
+
+        // crear instancias de Lavadora
+        Lavadora miLavadora1 = new Lavadora();
+        Lavadora miLavadora2 = new Lavadora(120, 50);
+        Lavadora miLavadora3 = new Lavadora(12);
+
+        //Crear instancias de Television
+        Television miTelevision1 = new Television();
+        Television miTelevision2 = new Television(180, 20);
+        Television miTelevision3 = new Television(65, true);
+
     }
 }
