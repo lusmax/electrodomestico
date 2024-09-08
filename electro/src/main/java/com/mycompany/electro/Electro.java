fbbdb5d1691da0666b4fc907d55d0ebaa3fbc1e1
lusmax <lusmax0128@gmail.com>
2024-09-09 00:07:46 +0200

    add intances to misElectrodomesticos

--- electro/src/main/java/com/mycompany/electro/Electro.java
+++ electro/src/main/java/com/mycompany/electro/Electro.java
@@ -9,7 +9,7 @@ import paq.*;
 public class Electro {
 
     public static void main(String[] args) {
-        Electrodomestico MisElectrodomesticos[] = new Electrodomestico[6];
+        Electrodomestico misElectrodomesticos[] = new Electrodomestico[6];
 
         // crear instancias de Lavadora
         Lavadora miLavadora1 = new Lavadora();
@@ -20,6 +20,14 @@ public class Electro {
         Television miTelevision1 = new Television();
         Television miTelevision2 = new Television(180, 20);
         Television miTelevision3 = new Television(65, true);
+
+        //Añadir Instancias(objetos) al array misElectrodomesticos
+        misElectrodomesticos[0] = miLavadora1;
+        misElectrodomesticos[1] = miLavadora2;
+        misElectrodomesticos[2] = miLavadora3;
+        misElectrodomesticos[3] = miTelevision1;
+        misElectrodomesticos[4] = miTelevision2;
+        misElectrodomesticos[5] = miTelevision3;
 
     }
 }
